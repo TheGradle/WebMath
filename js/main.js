0c75ef785f323c637296a397e845d6e05f04e7b6
thegradle <adamchuk.v.1@gmail.com>
2021-08-22 23:00:44 +0300

    Front-end

--- js/main.js
+++ js/main.js
@@ -2,9 +2,16 @@ var equation_span = document.getElementById("equation");
 var user_answer = document.getElementById("user-answer");
 var next_equation_btn = document.getElementById("next-equation");
 var result = document.getElementById("result");
+var equation_block = document.getElementById("equation-block");
 var result_block = document.getElementById("result-block");
 var refresh_btn = document.getElementById("refresh");
+var user_number = document.getElementById("number-of-equation");
+var settings_modal = document.getElementById("settings");
+var settings_open_btn = document.getElementById("open-settings");
+var settings_close_btn = document.getElementById("close-settings");
+var save_settings_btn = document.getElementById("save-settings");
 var counter = 0;
+var number = 10;
 
 function getRandomInt(max)
 {
@@ -39,7 +46,6 @@ document.addEventListener("DOMContentLoaded", function()
 
 next_equation_btn.addEventListener("click", function()
 {
-  var number = document.getElementById("number-of-equation").value;
   counter++;
   result.innerHTML += ((user_answer.value == user_answer.getAttribute("answer")) ? "✅ " : "❌ ") + equation_span.innerHTML + " = " + user_answer.value + "<br>";
 
@@ -48,9 +54,16 @@ next_equation_btn.addEventListener("click", function()
   }
   else {
     result_block.style.display = "block";
+    equation_block.style.display = "none";
   }
 
   user_answer.value = "";
+});
+
+save_settings_btn.addEventListener("click", function()
+{
+  number = user_number.value;
+  settings_modal.style.display = "none";
 });
 
 refresh_btn.addEventListener("click", function(){
@@ -62,5 +75,24 @@ user_answer.addEventListener("keyup", function(event)
   if (event.keyCode === 13) {
     event.preventDefault();
     next_equation_btn.click();
+  }
+});
+
+settings_open_btn.addEventListener("click", function()
+{
+  settings_modal.style.display = "block";
+
+  user_number.value = number;
+});
+
+settings_close_btn.addEventListener("click", function()
+{
+  settings_modal.style.display = "none";
+});
+
+window.addEventListener("click", function()
+{
+  if (event.target == settings_modal) {
+    settings_modal.style.display = "none";
   }
 });
